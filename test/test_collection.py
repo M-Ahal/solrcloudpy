ae2db352878b9fcf4371375aeed375e2bf0c4dc3
Mehul Ahal <m.ahal@dimerce.com>
2024-01-03 12:05:05 +0100

    [DIM-157] switched back to python 3.11.6 Because of 'imp' import deprecation

--- test/test_collection.py
+++ test/test_collection.py
@@ -11,14 +11,14 @@ solrprocess = None
 
 
 class TestCollectionAdmin(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.conn = SolrConnection(version=os.getenv("SOLR_VERSION", "6.1.0"))
         self.collparams = {}
         confname = os.getenv("SOLR_CONFNAME", "")
         if confname != "":
             self.collparams["collection_config_name"] = confname
 
-    def test_create_collection(self):
+    def test_create_collection(self) -> None:
         original_count = len(self.conn.list())
         coll2 = self.conn.create_collection("coll2", **self.collparams)
         time.sleep(3)
@@ -38,14 +38,14 @@ class TestCollectionAdmin(unittest.TestCase):
         time.sleep(3)
         self.assertEqual(len(self.conn.list()), original_count)
 
-    def test_reload(self):
+    def test_reload(self) -> None:
         coll2 = self.conn.create_collection("coll2", **self.collparams)
         time.sleep(3)
         res = coll2.reload()
         self.assertTrue(getattr(res, "success") is not None)
         coll2.drop()
 
-    def test_split_shard(self):
+    def test_split_shard(self) -> None:
         coll2 = self.conn.create_collection("coll2", **self.collparams)
         time.sleep(3)
         res = coll2.split_shard("shard1", ranges="80000000-90000000,90000001-7fffffff")
@@ -53,7 +53,7 @@ class TestCollectionAdmin(unittest.TestCase):
         self.assertTrue(getattr(res, "success") is not None)
         coll2.drop()
 
-    def test_create_shard(self):
+    def test_create_shard(self) -> None:
         coll2 = self.conn.create_collection(
             "coll2",
             router_name="implicit",
@@ -67,7 +67,7 @@ class TestCollectionAdmin(unittest.TestCase):
         self.assertTrue(getattr(res, "success") is not None)
         coll2.drop()
 
-    def test_create_delete_alias(self):
+    def test_create_delete_alias(self) -> None:
         coll2 = self.conn.create_collection("coll2", **self.collparams)
         coll2.create_alias("alias2")
         time.sleep(3)
@@ -75,7 +75,7 @@ class TestCollectionAdmin(unittest.TestCase):
         coll2.delete_alias("alias2")
         coll2.drop()
 
-    def test_delete_replica(self):
+    def test_delete_replica(self) -> None:
         try:
             coll2 = self.conn.create_collection(
                 "test_delete_replica",
@@ -92,15 +92,15 @@ class TestCollectionAdmin(unittest.TestCase):
             )
             coll2 = self.conn["test_delete_replica"]
         time.sleep(3)
-        firstReplica = list(coll2.shards["shards"]["myshard1"]["replicas"].dict.keys())[
+        first_replica = list(coll2.shards["shards"]["myshard1"]["replicas"].dict.keys())[
             0
         ]
-        result = coll2.delete_replica(firstReplica, "myshard1")
+        result = coll2.delete_replica(first_replica, "myshard1")
         self.assertTrue(result.success)
         coll2.drop()
 
 
-def setUpModule():
+def setUpModule() -> None:
     if os.getenv("SKIP_STARTUP", False):
         return
     solrprocess = SolrInstance("solr2")
@@ -109,7 +109,7 @@ def setUpModule():
     time.sleep(1)
 
 
-def tearDownModule():
+def tearDownModule() -> None:
     if os.getenv("SKIP_STARTUP", False):
         return
     if solrprocess:
